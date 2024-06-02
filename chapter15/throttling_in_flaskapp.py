3399576657b5a61c5c879de173b9e9dcfcb78901
lanceyliao <45159389+lanceyliao@users.noreply.github.com>
2024-06-02 04:23:41 +0000

    15.5

--- chapter15/throttling_in_flaskapp.py
+++ chapter15/throttling_in_flaskapp.py
@@ -5,8 +5,8 @@ from flask_limiter.util import get_remote_address
 from flask import Flask
 app = Flask(__name__)
 limiter = Limiter(
-    app,
-    key_func=get_remote_address,
+    get_remote_address,
+    app=app,
     default_limits=["100 per day", "10 per hour"]
 )
 
